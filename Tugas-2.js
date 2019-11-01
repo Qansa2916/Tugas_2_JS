5ea48309044916149936e48d95cfb44324816db7
Qansa Alghifari <46017824+Qansa2916@users.noreply.github.com>
2019-11-01 16:14:55 +0700

    Tugas Niomic
    Tugas JS Niomic

---
+++ Tugas-2.js
@@ -0,0 +1,7 @@
+var angka = 29;
+var tulisan = "Ini String";
+var benar = true;
+var kosong;
+var eweh = null;
+var deretan = [1, 2, 3, 4];
+console.log(angka, tulisan, benar, kosong, deretan, eweh);
